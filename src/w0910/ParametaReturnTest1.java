064ce04cced40d501bf3ba66022a70c8b8ee22e7
kjminn <kjminn2001@gmail.com>
2025-09-10 09:26:28 +0900

    리턴값과 매개변수 2개를 갖는 메소드호출 연습

---
+++ src/w0910/ParametaReturnTest1.java
@@ -0,0 +1,21 @@
+package w0910;
+
+import java.util.Scanner;
+
+public class ParametaReturnTest1 {
+
+    public static void main(String[] args) {
+        System.out.println("=== 2개 정수의 합계를 구해드립니다.===");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("* 정수1 입력:");
+        int num1 = sc.nextInt();
+        System.out.print("* 정수2입력:");
+        int num2 = sc.nextInt();
+
+        Calc1 calc1 = new Calc1();
+        int result = calc1.plus(num1, num2);
+        System.out.println("덧셈결과 = " + result);
+
+        sc.close();
+    }
+}
